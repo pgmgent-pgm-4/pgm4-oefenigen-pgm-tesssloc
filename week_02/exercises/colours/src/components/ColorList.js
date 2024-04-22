686198af4253608354c41494cac6e345a23f33ba
tesssloc <tess.slock@student.arteveldehs.be>
2024-04-22 11:29:21 +0200

    worked on colour exercise

--- week_02/exercises/colours/src/components/ColorList.js
+++ week_02/exercises/colours/src/components/ColorList.js
@@ -1,18 +1,14 @@
 import React, {useState} from 'react'
+import Color from './Color'
 
 export default function ColorList() {
-    const [colour, setColour] = useState(['red', 'green'])
-    const handleAddColour = (e) => {
-        setColour(colour.push(e.target.value))
+    const [listColors, setListColors] = useState(['rgb(10,30,55)', 'rgb(10,40,55)', 'rgb(30,30,55)'])
+    const generateRandomColor = () => {
+        return `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`
     }
   return (
     <div>
-        <ul>
-            {colour.map((item, index) => (
-            <li key={index}>{item}</li>
-            ))}
-        </ul>
-        <input id="color" type="text" onChange={handleAddColour}></input>
+        {listColors.map((color) => (<Color color={color}/>))}
     </div>
   )
 }
