e28cea0ac5cfdcd5e8fc4ecfe0866a7d44e7d92b
tesssloc <tess.slock@student.arteveldehs.be>
2024-04-23 14:50:09 +0200

    added frame works

--- week_02/exercises/css-modules/src/App.js
+++ week_02/exercises/css-modules/src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
 import TeamMember from './components/TeamMember';
 import TeamCaptain from './components/TeamCaptain';
+import Avatar from '@mui/material/Avatar';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 function App() {
   return (
     <div>
       <TeamMember img="https://pbs.twimg.com/profile_images/827161829843542017/4aDnLZeU_400x400.jpg" firstName="first" lastName="last" jobRole="role" email="mail" phoneNumber="23356"/>
+      <FormControl>
+      <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        defaultValue="female"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel value="female" control={<Radio />} label="Female" />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+      </RadioGroup>
+    </FormControl>
       <TeamCaptain img="https://pbs.twimg.com/profile_images/827161829843542017/4aDnLZeU_400x400.jpg" firstName="first" lastName="last" jobRole="role" email="mail" phoneNumber="23356"/>
+      <Avatar  alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+      <Button variant="contained">Hello world</Button>
+      <Button variant="outlined">Hello world</Button>
+ 
     </div>
   );
 }
--- week_02/exercises/light-dimmer/src/App.js
+++ week_02/exercises/light-dimmer/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import LightDimmer from './components/LightDimmer';
+import React from "react";
+import { Avatar } from "flowbite-react";
 
 function App() {
   return (
     <div className="App">
       <LightDimmer />
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Avatar img="https://pbs.twimg.com/profile_images/827161829843542017/4aDnLZeU_400x400.jpg" alt="avatar of Jese" />
     </div>
   );
 }
